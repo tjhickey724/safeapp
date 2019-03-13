8dc050c0e1b4d040853e7e19c5e2560799ecab48
tim <tjhickey@brandeis.edu>
2019-03-13 14:34:06 -0400

    moved pug files etc

--- app.js
+++ app.js
@@ -11,18 +11,11 @@ const
  //usersController = require('./controllers/usersController'),
  session = require("express-session"),
  bodyParser = require("body-parser"),
- User = require( './models/User' ),
+ //User = require( './models/User' ),
  flash = require('connect-flash')
 
-const Crime = require( './models/Crime' );
+//const Crime = require( './models/Crime' );
 
-
- var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
-
-  // here we set up authentication with passport
-  const passport = require('passport')
-  const configPassport = require('./config/passport')
-  configPassport(passport)
 
 
 var app = express();
@@ -58,8 +51,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(session({ secret: 'zzbbyanana789789t567556575' }));
 app.use(flash());
-app.use(passport.initialize());
-app.use(passport.session());
+//app.use(passport.initialize());
+//app.use(passport.session());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // this handles all static routes ...
@@ -68,105 +61,20 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 
 
-// here is where we check on their logged in status
-app.use((req,res,next) => {
-  res.locals.loggedIn = false
-  if (req.isAuthenticated()){
-    if (req.user.googleemail.endsWith("@brandeis.edu") ||
-          approvedLogins.includes(req.user.googleemail))
-          {
-            console.log("user has been Authenticated")
-            res.locals.user = req.user
-            res.locals.loggedIn = true
-          }
-    else {
-      res.locals.loggedIn = false
-    }
-    if (req.session.classV){
-      res.locals.classV=req.session.classV
-      res.locals.classId = req.session.classV._id
-    } else {
-      res.locals.classV = ""
-    }
-    if (req.user){
-      if (req.user.googleemail=='tjhickey@brandeis.edu'){
-        console.log("Owner has logged in")
-        res.locals.status = 'teacher'
-      } else if (taList.includes(req.user.googleemail)){
-        console.log("A TA has logged in")
-        res.locals.status = 'ta'
-      }else {
-        console.log('student has logged in')
-        res.locals.status = 'student'
-      }
-    }
-  }
-  next()
-})
-
-
-
-// here are the authentication routes
-
-app.get('/loginerror', function(req,res){
-  res.render('loginerror',{})
-})
-
-app.get('/login', function(req,res){
-  res.render('login',{})
-})
-
-
-
-// route for logging out
-app.get('/logout', function(req, res) {
-        req.session.destroy((error)=>{console.log("Error in destroying session: "+error)});
-        console.log("session has been destroyed")
-        req.logout();
-        res.redirect('/');
-    });
-
-
-// =====================================
-// GOOGLE ROUTES =======================
-// =====================================
-// send to google to do the authentication
-// profile gets us their basic information including their name
-// email gets their emails
-app.get('/auth/google', passport.authenticate('google', { scope : ['profile', 'email'] }));
-
-
-app.get('/login/authorized',
-        passport.authenticate('google', {
-                successRedirect : '/',
-                failureRedirect : '/loginerror'
-        })
-      );
-
 
 // route middleware to make sure a user is logged in
 function isLoggedIn(req, res, next) {
-    console.log("checking to see if they are authenticated!")
-    // if user is authenticated in the session, carry on
-    res.locals.loggedIn = false
-    if (req.isAuthenticated()){
-      console.log("user has been Authenticated")
-      res.locals.loggedIn = true
-      return next();
-    } else {
-      console.log("user has not been authenticated...")
-      res.redirect('/login');
-    }
+    return true;
 }
 
 app.get('/',function(req,res){
-  res.render("mainpage",{content:"main"})
+  res.render("index",{content:"main"})
     }
 )
 
 
 app.get('/addzip',function(req,res){
-  res.render("mainpage", {content:"addzip"})
+  res.render("index", {content:"addzip"})
     }
 )
 
