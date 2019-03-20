af490341e9e6c3c0d9235e1cc8cbd46ff356d8da
tim <tjhickey@brandeis.edu>
2019-03-20 15:01:03 -0400

    added axios with links to fbi crime database

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
+console.log("loading app.js")
 const
  createError = require('http-errors'),
  express = require('express'),
  path = require('path'),
  cookieParser = require('cookie-parser'),
  logger = require('morgan'),
+ axios = require('axios'),
  //classesController = require('./controllers/classesController'),
  //skillsController = require('./controllers/skillsController'),
  //evidenceController = require('./controllers/evidenceController'),
@@ -22,8 +24,8 @@ var app = express();
 
 app.locals._ = require("underscore");
 //app.locals._.uniq = require("underscore.unique");
-console.log("\nunderscore = "+app.locals._)
-console.log("uniq = "+app.locals._.uniq)
+//console.log("\nunderscore = "+app.locals._)
+//console.log("uniq = "+app.locals._.uniq)
 
 
 // skillsRouter = require('./routes/skills'),
@@ -67,11 +69,49 @@ function isLoggedIn(req, res, next) {
     return true;
 }
 
+
+
+app.get('/fbi',function(req,res){
+  console.log('inside app.get fbi')
+  const apikey = "gcjJlOUptmFW8r9ro441bqSNT3mzd4NN6JPbSz6Z"
+  const oriurl = "https://api.usa.gov/crime/fbi/sapi/api/agencies?api_key=iiHnOKfno2Mgkt5AynpvPpUQTEyxE77jo1RU8PIv"
+
+  let endpoint = "api/data/nibrs/robbery/victim/agencies/MA0132000/age"
+  let url2 = "https://api.usa.gov/crime/fbi/sapi/"+endpoint+"?api_key="+apikey
+
+  axios.get(url2)
+  .then(function (response) {
+    console.log("got the response");
+    res.send(response.data)
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+})
+
+app.get('/ori',function(req,res){
+  console.log('inside app.get fbi')
+  const apikey = "gcjJlOUptmFW8r9ro441bqSNT3mzd4NN6JPbSz6Z"
+  const oriurl = "https://api.usa.gov/crime/fbi/sapi/api/agencies?api_key=iiHnOKfno2Mgkt5AynpvPpUQTEyxE77jo1RU8PIv"
+
+  let endpoint = "api/data/nibrs/robbery/victim/agencies/MA0132000/age"
+  let url2 = "https://api.usa.gov/crime/fbi/sapi/"+endpoint+"?api_key="+apikey
+
+  axios.get(oriurl)
+  .then(function (response) {
+    console.log("got the response");
+    res.json(response.data)
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+})
+
 app.get('/',function(req,res){
+  console.log("you are looking at the right code!")
   res.render("index",{content:"main"})
     }
 )
-
 
 app.get('/addzip',function(req,res){
   res.render("index", {content:"addzip"})
